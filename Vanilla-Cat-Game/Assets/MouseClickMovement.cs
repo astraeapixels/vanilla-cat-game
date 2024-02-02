ec20b06e09571c02c98d4f7f6a186335b7b7bab0
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-02-02 00:38:48 -0500

    Made cat only be able to move across the x-axis

--- Vanilla-Cat-Game/Assets/MouseClickMovement.cs
+++ Vanilla-Cat-Game/Assets/MouseClickMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class MouseClickMovement : MonoBehaviour
 {
 
 public float speed = 10f;
-Vector2 lastClickedPos;
+private float lastClickedPos;
 bool moving;
 
 
@@ -18,16 +20,27 @@ bool moving;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            moving = true;
-        }
+        Vector2 target = new Vector2(lastClickedPos, 0.0f);
+        moving = true;
 
-        if(moving && (Vector2)transform.position != lastClickedPos){
+
+         if(Input.GetMouseButtonDown(0)){
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            
+         }
+      
+
+        if(moving && (Vector2)transform.position != target){
             float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
-        } else{
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+        }
+        else
+        {
             moving = false;
         }
     }
+
+    // void OnMouseDrag(){
+    //     transform.position = new UnityEngine.Vector3(lastClickedPos.x, transform.position.y, transform.position.z);
+    // }
 }
