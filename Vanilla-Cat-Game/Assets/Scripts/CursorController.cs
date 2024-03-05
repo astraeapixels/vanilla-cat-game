8eca1c499e8579789a976ba247708929847c8757
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-03-05 18:25:52 -0500

    Fixed Camera Bounds and added custom cursor

---
+++ Vanilla-Cat-Game/Assets/Scripts/CursorController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public static CursorController instance;
+
+    public Texture2D normalMouse;
+	public Texture2D pressedMouse;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void ActivateNormalCursor(){
+		Cursor.SetCursor(normalMouse, new Vector2(normalMouse.width / 2, normalMouse.height / 2), CursorMode.Auto);  
+	}
+    public void ActivatePressedCursor(){
+		Cursor.SetCursor(pressedMouse, new Vector2(normalMouse.width / 2, normalMouse.height / 2), CursorMode.Auto);  
+	}
+}
---
+++ Vanilla-Cat-Game/Assets/Scripts/Grabbable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+    private bool canGrab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canGrab){
+            if(Input.GetMouseButtonDown(0)){
+                CursorController.instance.ActivatePressedCursor();
+            }
+             if(Input.GetMouseButtonUp(0)){
+                CursorController.instance.ActivateNormalCursor();
+            }
+        }
+    }
+
+    private void OnMouseEnter(){
+        CursorController.instance.ActivateNormalCursor();
+
+        canGrab = true;
+    }
+
+    private void OnMouseExit(){
+        //canGrab = false;
+    }
+}
