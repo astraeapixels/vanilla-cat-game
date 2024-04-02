ccde3fea764858aac5e8df09cabd05ddeffee466
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-04-02 02:55:28 -0400

    Added a simple dialogue system and UI elements

---
+++ Vanilla-Cat-Game/Assets/Scripts/UI Scripts/UIDialoguePrefab.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIDialoguePrefab : MonoBehaviour
+{
+    [SerializeField, TextArea(1,6)] private string[] dialogueLines;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private GameObject dialoguePanel;
+
+    private bool didDialoguePlay;
+    private int stringIndex;
+    private float typingTime = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!didDialoguePlay)
+            {
+                StartDialogue();
+            }else if(dialogueText.text == dialogueLines[stringIndex])
+            {
+                NextLineInDialogue();
+            }else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[stringIndex];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialoguePlay = true;
+        dialoguePanel.SetActive(true);
+        stringIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+    private void NextLineInDialogue()
+    {
+        stringIndex++;
+        if(stringIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }else
+        {
+            didDialoguePlay = false;
+            dialoguePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[stringIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+}
