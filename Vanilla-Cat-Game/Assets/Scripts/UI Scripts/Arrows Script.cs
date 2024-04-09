48400b3c57fa684b15bc9b376856bf5bf5ff4deb
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-04-09 15:04:44 -0400

    Added Show Next and Go Back for Customization UI

---
+++ Vanilla-Cat-Game/Assets/Scripts/UI Scripts/Arrows Script.cs
@@ -0,0 +1,45 @@
+
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowsScript : MonoBehaviour
+{
+    [SerializeField] private Image[] previewImages;
+    [SerializeField] Button leftButton, rightButton;
+    [SerializeField] private int numOfHatImages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightButton.onClick.AddListener(showNextPreview);
+        leftButton.onClick.AddListener(showPreviousPreview);
+    }
+
+    // Update is called once per frame
+    public void showNextPreview()
+    {
+        numOfHatImages++;
+
+        if(numOfHatImages > previewImages.Length -1)
+        {
+            numOfHatImages = 0;
+        }
+
+        previewImages[numOfHatImages].enabled = true;
+    }
+
+    public void showPreviousPreview()
+    {
+    
+    previewImages[numOfHatImages].enabled = false;
+    numOfHatImages--;
+    if(numOfHatImages < 0)
+    {
+        numOfHatImages = previewImages.Length -1;
+    }
+
+    // Show previous model
+    previewImages[numOfHatImages].enabled = true;
+    }
+}
