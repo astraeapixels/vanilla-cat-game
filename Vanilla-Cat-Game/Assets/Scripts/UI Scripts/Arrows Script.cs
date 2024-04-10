88f8e3f2edc32dea58863633208994b7715246fd
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-04-10 02:35:07 -0400

    Arrow Script is a WIP (have no idea what I'm doing)

--- Vanilla-Cat-Game/Assets/Scripts/UI Scripts/Arrows Script.cs
+++ Vanilla-Cat-Game/Assets/Scripts/UI Scripts/Arrows Script.cs
@@ -1,5 +1,6 @@
 
 using Unity.VisualScripting;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,13 @@ public class ArrowsScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+    //     for(int i = 0; i < previewImages.Length; i++)
+    // {
+    //     if(!previewImages[i].isActiveAndEnabled) continue; 
+
+    //     numOfHatImages = i;
+    // }
         rightButton.onClick.AddListener(showNextPreview);
         leftButton.onClick.AddListener(showPreviousPreview);
     }
@@ -19,7 +26,7 @@ public class ArrowsScript : MonoBehaviour
     // Update is called once per frame
     public void showNextPreview()
     {
-        previewImages[numOfHatImages].enabled = false;
+        previewImages[numOfHatImages].enabled = true;
         numOfHatImages++;
 
         if(numOfHatImages > previewImages.Length -1)
@@ -27,20 +34,19 @@ public class ArrowsScript : MonoBehaviour
             numOfHatImages = 0;
         }
 
-        previewImages[numOfHatImages].enabled = true;
+        //previewImages[numOfHatImages].enabled = false;
     }
 
     public void showPreviousPreview()
     {
-    
-    previewImages[numOfHatImages].enabled = false;
-    numOfHatImages--;
-    if(numOfHatImages < 0)
-    {
-        numOfHatImages = previewImages.Length -1;
-    }
+        previewImages[numOfHatImages].enabled = true;
+        numOfHatImages--;
+        if(numOfHatImages < 0)
+        {
+            numOfHatImages = previewImages.Length -1;
+        }
 
-    // Show previous model
-    previewImages[numOfHatImages].enabled = true;
+        // Show previous model
+        previewImages[numOfHatImages].enabled = true;
     }
-}
+    }
