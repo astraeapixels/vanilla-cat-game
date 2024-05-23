25cdbdcda9f0ad4d450c937460100207d51e6b01
astraeapixels <139937168+astraeapixels@users.noreply.github.com>
2024-05-23 19:23:24 -0400

    formatted code a bit?? sorry I am just committing anything at this point... T.T

--- Vanilla-Cat-Game/Assets/Scripts/UI Scripts/Cinematic Bars.cs
+++ Vanilla-Cat-Game/Assets/Scripts/UI Scripts/Cinematic Bars.cs
@@ -7,20 +7,20 @@ public class CinematicBars : MonoBehaviour
     [SerializeField] private float fadeTime;
 
         public void ShowBars(){
-        topBar.transform.localPosition = new Vector3(0f, 198f, 0f);
-        Tween.UIAnchoredPosition(topBar, new Vector2(0f, 162f), fadeTime, ease: Ease.InOutQuint, 1);
+            topBar.transform.localPosition = new Vector3(0f, 198f, 0f);
+            Tween.UIAnchoredPosition(topBar, new Vector2(0f, 162f), fadeTime, ease: Ease.InOutQuint, 1);
 
-        bottomBar.transform.localPosition = new Vector3(0f, -198f, 0f);
-        Tween.UIAnchoredPosition(bottomBar, new Vector2(0f, -162f), fadeTime, ease: Ease.InOutQuint, 1);
-        
-    }
+            bottomBar.transform.localPosition = new Vector3(0f, -198f, 0f);
+            Tween.UIAnchoredPosition(bottomBar, new Vector2(0f, -162f), fadeTime, ease: Ease.InOutQuint, 1);
+            }
 
         public void HideBars()
         {
-        topBar.transform.localPosition = new Vector3(0f, 198f, 0f);
-        Tween.UIAnchoredPosition(topBar, new Vector2(0f, 198f), fadeTime, ease: Ease.InOutQuint, 1);
+            topBar.transform.localPosition = new Vector3(0f, 198f, 0f);
+            Tween.UIAnchoredPosition(topBar, new Vector2(0f, 198f), fadeTime, ease: Ease.InOutQuint, 1);
 
-        bottomBar.transform.localPosition = new Vector3(0f, -198f, 0f);
-        Tween.UIAnchoredPosition(bottomBar, new Vector2(0f, -198f), fadeTime, ease: Ease.InOutQuint, 1);
+            bottomBar.transform.localPosition = new Vector3(0f, -198f, 0f);
+            Tween.UIAnchoredPosition(bottomBar, new Vector2(0f, -198f), fadeTime, ease: Ease.InOutQuint, 1);
+
         }
     }
